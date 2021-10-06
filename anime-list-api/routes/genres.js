3567aa1d5b1d31ea550633344c12a2bd10d835d1
Xenia <xenia.hahn@gmx.de>
2021-10-06 20:33:41 +0200

    #reorganizing-backend-strucutre-split-up-routes-into-single-files

---
+++ anime-list-api/routes/genres.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../app/models'); 
+const genre = models.genre;     
+
+/**
+* @swagger
+* /genres:
+*  get:
+*    summary: Use to request all genres
+*    responses:
+*      200:
+*        description: collection of all stored genres
+*/
+
+router.get('/genres', (req, res) => {
+ genre.findAll().then((genre) => {
+   res.json(genre);
+ })
+}); 
+
+/**
+ * @swagger
+ * /genres:
+ *  post:
+ *    summary: Use to insert new genre
+ *    responses:
+ *      200:
+ *        description: insert new genre
+ */
+
+ router.post('/genres', (req, res) => {
+  const newGenre = new Genre({
+    title: req.body.title,
+  })
+  newGenre.save();
+});
+
+router.put('/genre/:genreId', (req, res) => {
+  genre.findByPk(req.params.genreId)
+  .then(genre => {
+    if (!genre) {
+      return res.status(404).send({
+        message: 'Genre Not Found',
+      });
+    }
+    return genre.update({
+      title: req.body.title,
+    })
+    .then(() => res.status(200).send(genre))
+    .catch((error) => res.status(400).send(error));
+  })
+  .catch(error => res.status(400).send(error));
+});
+
+router.get('/genre/:genreId', (req, res) => {
+  genre.findByPk(req.params.genreId)
+  .then(genre => {
+    if (!genre) {
+      return res.status(404).send({
+        message: 'Genre Not Found',
+      });
+    }
+    return res.status(200).send(genre);
+  })
+  .catch(error => res.status(400).send(error));
+});
+
+router.delete('/genre/:genreId', (req, res) => {
+  genre.findByPk(req.params.genreId)
+  .then(genre => {
+    if (!genre) {
+      return res.status(404).send({
+        message: 'Genre Not Found',
+      });
+    }
+    return genre.destroy()
+    .then(() => res.status(204).send())
+    .catch((error) => res.status(400).send(error));
+  })
+  .catch(error => res.status(400).send(error));
+});
+
+module.exports = router;
+  
--- anime-list-api/server.js
+++ anime-list-api/server.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+var indexRouter = require('./routes/index');
+var animeRouter = require('./routes/animes');
+var genreRouter = require('./routes/genres');
 
 //for building rest apis
 const express = require("express");
@@ -22,6 +25,7 @@ db.sequelize.sync();
 //app.use(bodyParser.json()); deprectated. now use this: (body parsing has become builtin with express)
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(indexRouter, animeRouter, genreRouter);
 
 const swaggerOptions = {
   swaggerDefinition: {
@@ -44,222 +48,13 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-
-
-//simple route
-app.get("/", (req, res) => {
-    res.json({ 
-      message: "Welcome to anime-list backend. Seeing this message means it is working!" 
-    });
-});
-
-//animes
-/**
- * @swagger
- * /animes:
- *  get:
- *    summary: Use to request all animes
- *    responses:
- *      200:
- *        description: collection of all stored animes
- */
-app.get("/animes", (req, res) => {
-  anime.findAll().then((anime) => {
-    res.json(anime);
-  })
-}); 
-
-const Anime = db.anime;
-
-/**
- * @swagger
- * /animes:
- *  post:
- *    summary: Use to insert new anime
- *    responses:
- *      200:
- *        description: store new anime
- */
-
-app.post("/animes", (req, res) => {
-  const newAnime = new Anime({
-    title_jp: req.body.title_jp,
-    title_eng: req.body.title_eng, 
-    status: req.body.status, 
-    end_date: req.body.end_date, 
-    studio: req.body.studio, 
-    source: req.body.source, 
-    current_episode_downloaded: req.body.current_episode_downloaded, 
-    current_episode_watched: req.body.current_episode_watched, 
-    total_number_episodes: req.body.total_number_episodes, 
-    release_year: req.body.release_year, 
-    rating: req.body.rating,  
-    releaseSeasonId: req.body.releaseSeasonId, 
-    release_season_id: req.body.release_season_id, 
-    groupId: req.body.groupId, 
-    group_id: req.body.group_id
-  })
-  newAnime.save();
-});
-
-//finde one
-app.get("/anime/:animeId", (req, res) => {
-  anime.findByPk(req.params.animeId)
-  .then(anime => {
-    if (!anime) {
-      return res.status(404).send({
-        message: 'Anime Not Found',
-      });
-    }
-    return res.status(200).send(anime);
-  })
-  .catch(error => res.status(400).send(error));
-});
-
-//update
-app.put("/anime/:animeId", (req, res) => {
-  anime.findByPk(req.params.animeId)
-  .then(anime => {
-    if (!anime) {
-      return res.status(404).send({
-        message: 'Anime Not Found',
-      });
-    }
-    return anime.update({
-      title_jp: req.body.title_jp,
-      title_eng: req.body.title_eng, 
-      status: req.body.status, 
-      end_date: req.body.end_date, 
-      studio: req.body.studio, 
-      source: req.body.source, 
-      current_episode_downloaded: req.body.current_episode_downloaded, 
-      current_episode_watched: req.body.current_episode_watched, 
-      total_number_episodes: req.body.total_number_episodes, 
-      release_year: req.body.release_year, 
-      rating: req.body.rating,  
-      releaseSeasonId: req.body.releaseSeasonId, 
-      release_season_id: req.body.release_season_id, 
-      groupId: req.body.groupId, 
-      group_id: req.body.group_id
-    })
-    .then(() => res.status(200).send(anime))
-    .catch((error) => res.status(400).send(error));
-  })
-  .catch(error => res.status(400).send(error));
-});
-
-//delete
-
-app.delete("/anime/:animeId", (req, res) => {
-  anime.findByPk(req.params.animeId)
-  .then(anime => {
-    if (!anime) {
-      return res.status(404).send({
-        message: 'Anime Not Found',
-      });
-    }
-    return anime.destroy()
-    .then(() => res.status(204).send())
-    .catch((error) => res.status(400).send(error));
-  })
-  .catch(error => res.status(400).send(error));
-});
-
-//genres
-
-const Genre = db.genre;
-
-/**
- * @swagger
- * /genres:
- *  get:
- *    summary: Use to request all genres
- *    responses:
- *      200:
- *        description: collection of all stored genres
- */
-
-app.get("/genres", (req, res) => {
-  genre.findAll().then((genre) => {
-    res.json(genre);
-  })
-}); 
-
-/**
- * @swagger
- * /genres:
- *  post:
- *    summary: Use to insert new genre
- *    responses:
- *      200:
- *        description: insert new genre
- */
-
-app.post("/genres", (req, res) => {
-  const newGenre = new Genre({
-    title: req.body.title,
-  })
-  newGenre.save();
-});
-
-//update
-
-app.put("/genre/:genreId", (req, res) => {
-  genre.findByPk(req.params.genreId)
-  .then(genre => {
-    if (!genre) {
-      return res.status(404).send({
-        message: 'Genre Not Found',
-      });
-    }
-    return genre.update({
-      title: req.body.title,
-    })
-    .then(() => res.status(200).send(genre))
-    .catch((error) => res.status(400).send(error));
-  })
-  .catch(error => res.status(400).send(error));
-});
-
-//delete
-
-app.delete("/genre/:genreId", (req, res) => {
-  genre.findByPk(req.params.genreId)
-  .then(genre => {
-    if (!genre) {
-      return res.status(404).send({
-        message: 'Genre Not Found',
-      });
-    }
-    return genre.destroy()
-    .then(() => res.status(204).send())
-    .catch((error) => res.status(400).send(error));
-  })
-  .catch(error => res.status(400).send(error));
-});
-
-//finde one
-app.get("/genre/:genreId", (req, res) => {
-  genre.findByPk(req.params.genreId)
-  .then(genre => {
-    if (!genre) {
-      return res.status(404).send({
-        message: 'Genre Not Found',
-      });
-    }
-    return res.status(200).send(genre);
-  })
-  .catch(error => res.status(400).send(error));
-});
-
 //listen for request, setting the port
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-const { anime, genre } = require("./app/models");
-db.sequelize.sync();
+const { genre } = require("./app/models");
 
 
 
