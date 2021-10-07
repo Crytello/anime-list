ebb149284d945c70b2e6f1b5154aa0b5a9b2703e
Xenia Hahn <xenia.hahn@deutschebahn.com>
2021-10-07 17:06:40 +0200

    #main-prepare-router-testing-with-jest

--- anime-list-api/app/tests/router-anime.test.js
+++ anime-list-api/app/tests/router-anime.test.js
@@ -1,16 +1,22 @@
 const request = require('supertest');
 const app = require('../../app');
 
-xdescribe('Anime Endpoints', () => {
-  xit('should get all animes', async () => {
+fdescribe('Anime Endpoints', () => {
+  fit('should get all animes', async () => {
     const res = await request(app).get('/animes');
+
+    expect(res).toBeDefined();
     expect(res.statusCode).toEqual(200);
-    expect(res.body.message).toEqual('Welcome to anime-list api. Seeing this message means it is working!');
+    //expect(res.text).toBe()
+    //expect(res.text).toHaveProperty('title_eng');
   });
   xit('should create a new anime', async () => {
-    const res = await request(app).post('/animes');
+    const res = await request(app).post('/animes').send({
+
+    });
+    expect(res).toBeDefined();
     expect(res.statusCode).toEqual(200);
-    expect(res.body.message).toEqual('Welcome to anime-list api. Seeing this message means it is working!');
+    //expect(res.body.message).toEqual('Welcome to anime-list api. Seeing this message means it is working!');
   });
   xit('should get an single anime as response', async () => {
     const res = await request(app).get('/anime');
