c7bf68e716ec5dbbbcb693f78ccfd1f58335d56d
Xenia <xenia.hahn@gmx.de>
2022-01-25 20:31:06 +0100

    #main-reorganize-manga-table-view

--- anime-list-frontend/src/components/MangaComponent/MangaListTableView.vue
+++ anime-list-frontend/src/components/MangaComponent/MangaListTableView.vue
@@ -1,7 +1,7 @@
 <template>
-    <table class="table">
-  <thead>
-    <tr>
+  <table class="table">
+    <thead>
+      <tr>
         <th scope="col">Löschen</th>
         <th scope="col">Bearbeiten</th>
         <th scope="col">Japanischer Titel / title_jp</th>
@@ -14,29 +14,28 @@
         <th scope="col">Gesamtanzahl Bände</th>
         <th scope="col">Releasejahr</th>
         <th scope="col">Bewertung / Rating</th>
-    </tr>
-  </thead>
-  <tbody v-for="manga in mangas" v-bind:key="manga.title_jp">
-    <tr>
-        <td><button @click="deleteManga(manga)">Löschen</button></td>
-        <td><button @click="updateManga(manga)">Bearbeiten</button></td>
-        <td><input v-model="manga.title_jp"/></td>
-        <td><input v-model="manga.title_eng"/></td>
-        <td><input v-model="manga.title_de"/></td>
-        <td><input v-model="manga.status"/></td>
-        <td><input v-model="manga.end_date"/></td>
-        <td><input v-model="manga.publisher"/></td>
-        <td><input v-model="manga.current_volume"/></td>
-        <td><input v-model="manga.total_number_volumes"/></td>
-        <td><input v-model="manga.release_year"/></td>
-        <td><input v-model="manga.rating"/></td>
-    </tr>
-  </tbody>
-</table>
+      </tr>
+    </thead>
+    <tbody v-for="manga in mangas" v-bind:key="manga.title_jp">
+      <tr>
+        <td><button class="button"  @click="deleteManga(manga)">Löschen</button></td>
+        <td><button class="button" @click="updateManga(manga)">Bearbeiten</button></td>
+        <td>{{manga.title_jp}}</td>
+        <td>{{manga.title_eng}}</td>
+        <td>{{manga.title_de}}</td>
+        <td>{{manga.status}}</td>
+        <td>{{manga.end_date}}</td>
+        <td>{{manga.publisher}}</td>
+        <td>{{manga.current_volume}}</td>
+        <td>{{manga.total_number_volumes}}</td>
+        <td>{{manga.release_year}}</td>
+        <td>{{manga.rating}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-
 export default({
   props: ['mangas', 'deleteManga', 'updateManga'],
 })
